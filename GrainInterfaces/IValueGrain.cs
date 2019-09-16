d4d54a4c2320e02540e9c1fd84d3526a2f7f0164
Duho Wise <Duhowise@gmail.com>
2019-09-16 15:51:32 +0000

    Added Value grain and coresponding interface

---
+++ GrainInterfaces/IValueGrain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orleans;
+
+namespace GrainInterfaces
+{
+    public interface IValueGrain: IGrainWithIntegerKey
+    {
+        Task<string> GetValue();
+        Task SetValue(string value);
+    }
+}
---
+++ Grains/ValueGrain.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using GrainInterfaces;
+using Orleans;
+
+namespace Grains
+{
+    public class ValueGrain:Grain,IValueGrain
+    {
+        private string _value;
+        public Task<string> GetValue()
+        {
+            return Task.FromResult(_value);
+        }
+
+        public Task SetValue(string value)
+        {
+            _value = value;
+            return Task.CompletedTask;
+        }
+    }
+}
