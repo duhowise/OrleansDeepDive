b12c2a88ff06d33460797cca8d0443a92be0b05c
Duho Wise <Duhowise@gmail.com>
2019-09-16 16:37:24 +0000

    Added Ado dotnet persistence

--- Silo/Program.cs
+++ Silo/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Hosting;
+using Orleans.Logging;
 
 namespace Silo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var siloHostBuilder = new SiloHostBuilder()
+                .UseLocalhostClustering(serviceId: "localhostCart")
+                .AddAdoNetGrainStorage("OrleansStorage", options =>
+                {
+                    options.Invariant = "";
+                    options.UseJsonFormat = true;
+                    options.ConnectionString = "Server=.;Database=OrleansDemo;Trusted_Connection=True;";
+                })
+                .ConfigureLogging(x=>x.AddConsole())
+                .UseDashboard();
+            using (var host=siloHostBuilder.Build())
+            {
+                await host.StartAsync();
+                Console.ReadLine();
+            }
         }
     }
 }
